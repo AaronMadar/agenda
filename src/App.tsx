af1c103b891afe5c4e23cb380189824810795858
avi <a0548589558@gmail.com>
2026-01-22 10:42:08 +0200

    initialized the basic card of dashboard-commander

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import GantPage from '@/pages/GantPage'
+import { Dashboard } from '@/pages/Dashboard'
 
 
 export default function App() {
     return (
         <Routes>
             <Route path="/" element={<GantPage />} />
+            <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
     )
 }
---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,12 @@
+import { Header } from "@/components/dashboard/Header";
+import { Body } from "@/components/dashboard/Body";
+
+
+export const Dashboard = () => {
+    return (
+        <div>
+            <Header />
+            <Body />
+        </div>
+    );
+}

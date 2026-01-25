d000301bb99bebf753f3683f1be91f046bea2cee
avi <a0548589558@gmail.com>
2026-01-25 12:20:11 +0200

    reports component has been complited, the global scroller has been styled as well

--- src/components/dashboard/reports/Reports.tsx
+++ src/components/dashboard/reports/Reports.tsx
@@ -1,10 +1,18 @@
+import style from "@/style/components/dashboard/reports/Reports.module.css"
 
 
-
-export const Reports = () => {
-    return(
-        <div>
-            Reports
-        </div>
-    )
+interface ReportsProps {
+    reports: string[];
 }
+
+export const Reports = ({ reports }: ReportsProps) => {
+    return (
+        <ul className={style.list}>
+            {reports.map((report, index) => (
+                <li key={index} className={style.item}>
+                    {report}
+                </li>
+            ))}
+        </ul>
+    );
+};

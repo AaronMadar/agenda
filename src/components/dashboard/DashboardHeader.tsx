bd4b57dbdc427f87c7aede3d51a549964d99d069
avi <a0548589558@gmail.com>
2026-01-27 17:08:30 +0200

    j;lkjjjkkkk

--- src/components/dashboard/DashboardHeader.tsx
+++ src/components/dashboard/DashboardHeader.tsx
@@ -1,15 +1,28 @@
+import { useNavigate } from 'react-router-dom';
 import SelectTime from '../gantpage/header/SelectTime';
 import style from "@/style/components/dashboard/DashboardHeader.module.css"
 
 
 export const DashboardHeader = () => {
+    const navigate = useNavigate();
+    
     return (
         <div className={style.containerWrapper}>
             <h4 style={{flex: 1}}>דשבורד למפקד</h4>
             <div className={style.controlsContainer}>
                 <SelectTime />
-                <img src="/dashboard-image-gray.png" alt="Dashboard Image" className={style.iconImage}></img>
-                <img src="/dashboard-image-blue.png" alt="Dashboard Image" className={style.iconImage}></img>
+                <img 
+                    src="/dashboard-image-gray.png" 
+                    alt="Dashboard Image" 
+                    className={style.iconImage}
+                    onClick={() => navigate('/')}
+                />
+                <img 
+                    src="/dashboard-image-blue.png" 
+                    alt="Dashboard Image" 
+                    className={style.iconImage}
+                    onClick={() => navigate('/')}
+                />
             </div>
         </div>
     );

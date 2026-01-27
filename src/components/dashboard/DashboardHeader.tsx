d890cb92e46ecd69246c2bc17dcf064991c03d89
avi <a0548589558@gmail.com>
2026-01-27 16:42:30 +0200

    fixed css properteis to fix object locations

--- src/components/dashboard/DashboardHeader.tsx
+++ src/components/dashboard/DashboardHeader.tsx
@@ -5,7 +5,7 @@ import style from "@/style/components/dashboard/DashboardHeader.module.css"
 export const DashboardHeader = () => {
     return (
         <div className={style.containerWrapper}>
-            <h2 style={{flex: 1}}>דשבורד למפקד</h2>
+            <h4 style={{flex: 1}}>דשבורד למפקד</h4>
             <div className={style.controlsContainer}>
                 <SelectTime />
                 <img src="/dashboard-image-gray.png" alt="Dashboard Image" className={style.iconImage}></img>
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -5,7 +5,7 @@ import { DashboardBody } from "@/components/dashboard/DashboardBody";
 export const Dashboard = () => {
     const style: React.CSSProperties = {
         background: "radial-gradient(circle,rgba(46, 53, 66, 1) 0%, rgba(25, 28, 28, 1) 64%)",
-        minHeight: "100vh",
+        height: "100vh",
         color: "white",
         display: "flex",
         flexDirection: "column",

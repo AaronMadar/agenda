a1e3a34115183a1d834d7aa823398ba32a030972
avi <a0548589558@gmail.com>
2026-02-04 18:11:24 +0200

    complited the month peacker

--- src/utils/calendar/extractCalendarEvents.ts
+++ src/utils/calendar/extractCalendarEvents.ts
@@ -1,21 +1,24 @@
-import type { CalendarSourceData, CalendarEvent } from "@/components/dashboard/training-calendar/types";
+import type {
+  CalendarSourceData,
+  CalendarEvent,
+} from "@/components/dashboard/training-calendar/types";
 
 export const extractCalendarEvents = (
-  data: CalendarSourceData
+  data: CalendarSourceData,
 ): CalendarEvent[] => {
   const events: CalendarEvent[] = [];
 
-  data.gdudim.forEach(gdud => {
-    gdud.shibutsim.forEach(sh => {
+  data.gdudim.forEach((gdud) => {
+    gdud.shibutsim.forEach((sh) => {
       let current = new Date(sh.dateBegin);
       const end = new Date(sh.dateEnd);
 
       while (current <= end) {
         events.push({
-        date: current.toISOString().slice(0, 10),
-        title: sh.title,
-        gdud: gdud.name,
-        serviceType: sh.seviceType,
+          date: current.toISOString().slice(0, 10),
+          title: sh.title,
+          gdud: gdud.name,
+          serviceType: sh.seviceType,
         });
 
         current.setDate(current.getDate() + 1);

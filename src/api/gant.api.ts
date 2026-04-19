a13655394fbc4dfc6f95fac346d6ac5e6ff8e150
Aaron <aaronmadar2655@gmail.com>
2026-04-19 10:18:27 +0300

    last fix

--- src/api/gant.api.ts
+++ src/api/gant.api.ts
@@ -5,16 +5,16 @@ export const getShibutzimData = async (
   from: string,
   to: string,
   unitIds: string[],
-  serviceTypes: string[] | null = null,
-  resourceTypes: string[] | null = null,
+  serviceTypeIds: string[] | null = null,
+  resourceTypeIds: string[] | null = null,
 ): Promise<ShibutzimRes> => {
   const { data } = await httpClient.get("/shibutzim", {
     params: {
       from,
       to,
       unitIds,
-      serviceTypeIds: serviceTypes,
-      resourceTypeIds: resourceTypes,
+      serviceTypeIds,
+      resourceTypeIds,
     },
   });
   return data;

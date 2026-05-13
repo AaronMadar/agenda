ee149221e35a024c72eb8b3bc3081bb150550c8b
AviGoldshtein <a0548589558@gmail.com>
2026-05-13 17:16:47 +0300

    remove unused imports and warn on unused params

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import unusedImports from 'eslint-plugin-unused-imports';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -22,10 +23,21 @@ export default defineConfig([
     },
     plugins: {
       'simple-import-sort': simpleImportSort,
+      'unused-imports': unusedImports,
     },
     rules: {
       'simple-import-sort/imports': 'error',
       'simple-import-sort/exports': 'error',
+
+      'unused-imports/no-unused-imports': 'error',
+
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
     },
   },
 ]);

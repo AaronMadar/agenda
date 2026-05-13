7548fe71ad6272ae2d6f5d0d8ae742d89a2286f4
AviGoldshtein <a0548589558@gmail.com>
2026-05-13 13:53:56 +0300

    removed unused css classes

--- src/components/dashboard/quantity-and-cost/QuantityCostCard.tsx
+++ src/components/dashboard/quantity-and-cost/QuantityCostCard.tsx
@@ -43,9 +43,7 @@ export const QuantityCostCard = ({
           {loading ? (
             <LoaderCircle size={20}/>
           ) : (
-            <div className={style.value}>
-                {formatValue(total)}
-            </div>
+            <div>{formatValue(total)}</div>
           )}
         </div>
       </div>
